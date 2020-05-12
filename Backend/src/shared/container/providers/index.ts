0475a164f5d2b030940e924ce82be0f6733631b1
Stephany <tete.1818@hotmail.com>
2020-05-11 22:25:02 -0300

    Template Email

--- Backend/src/shared/container/providers/index.ts
+++ Backend/src/shared/container/providers/index.ts
@@ -6,9 +6,17 @@ import DiskStorageProvider from './StorageProvider/implementations/DiskStoragePr
 import IMailProvider from './MailProvider/models/IMailProvider';
 import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
 
+import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
+import HandlebarMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarMailTemplateProvider';
+
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
+);
+
+container.registerSingleton<IMailTemplateProvider>(
+  'MailTemplateProvider',
+  HandlebarMailTemplateProvider,
 );
 
 container.registerInstance<IMailProvider>(

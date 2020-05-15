40b0c2bc2857283f059eb9601c7564f07db96c35
Stephany <tete.1818@hotmail.com>
2020-05-14 21:56:03 -0300

    Redis

--- Backend/src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ Backend/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import { injectable, inject } from 'tsyringe';
 
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 import Appontment from '../infra/typeorm/entities/Appointment';
 
@@ -16,6 +17,9 @@ class ListProviderAppointmentsService {
   constructor(
     @inject('AppontimentsRepository')
     private appointmentsRepository: IAppointmentsRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({
@@ -24,6 +28,8 @@ class ListProviderAppointmentsService {
     month,
     day,
   }: IRequest): Promise<Appontment[]> {
+    const cacheData = await this.cacheProvider.recover('asd');
+    console.log(cacheData);
     const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
       {
         provider_id,
@@ -32,6 +38,8 @@ class ListProviderAppointmentsService {
         day,
       },
     );
+
+    // await this.cacheProvider.save('asd', 'asd');
 
     return appointments;
   }
---
+++ Backend/src/shared/container/providers/MailTemplateProvider/index.ts
@@ -0,0 +1,13 @@
+import { container } from 'tsyringe';
+
+import IMailTemplateProvider from './models/IMailTemplateProvider';
+import HandlebarMailTemplateProvider from './implementations/HandlebarMailTemplateProvider';
+
+const providers = {
+  handlebars: HandlebarMailTemplateProvider,
+};
+
+container.registerSingleton<IMailTemplateProvider>(
+  'MailTemplateProvider',
+  providers.handlebars,
+);

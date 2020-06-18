9ed50073d25f44ceb2c0dcfa6b498e7cb7ce77df
Stephany <tete.1818@hotmail.com>
2020-06-18 18:44:49 -0300

    Dashboard and API tuning

---
+++ appgobarber/src/pages/CreateAppointment/index.tsx
@@ -0,0 +1,102 @@
+import React, { useCallback, useEffect, useState } from 'react';
+
+import { useRoute, useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
+import {
+  Container,
+  Header,
+  BackButton,
+  HeaderTitle,
+  UserAvatar,
+  ProvidersListContainer,
+  ProvidersList,
+  ProviderContainer,
+  ProviderAvatar,
+  ProviderName,
+} from './styles';
+import { useAuth } from '../../hooks/auth';
+import api from '../../services/api';
+
+export interface Provider {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
+
+interface RouteParams {
+  providerId: string;
+}
+
+const CreateAppointment: React.FC = () => {
+  const { user } = useAuth();
+  const route = useRoute();
+  const { goBack } = useNavigation();
+  const RouteParams = route.params as RouteParams;
+
+  const [providers, setProviders] = useState<Provider[]>([]);
+  const [selectedProvider, setSelectedProvider] = useState(
+    RouteParams.providerId,
+  );
+
+  useEffect(() => {
+    api.get('providers').then((response) => {
+      setProviders(response.data);
+    });
+  }, []);
+
+  const navigateBack = useCallback(() => {
+    goBack();
+  }, [goBack]);
+
+  const handleSelecteProvider = useCallback((providerId: string) => {
+    setSelectedProvider(providerId);
+  }, []);
+
+  return (
+    <Container>
+      <Header>
+        <BackButton onPress={navigateBack}>
+          <Icon name="chevron-left" size={24} color="#999591" />
+        </BackButton>
+
+        <HeaderTitle>Cabeleireiros</HeaderTitle>
+
+        <UserAvatar
+          source={{
+            uri:
+              user.avatar_url ||
+              `https://api.adorable.io/avatar/50/${user.name}.jpeg`,
+          }}
+        />
+      </Header>
+
+      <ProvidersListContainer>
+        <ProvidersList
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          data={providers}
+          keyExtractor={(provider) => provider.id}
+          renderItem={({ item: provider }) => (
+            <ProviderContainer
+              onPress={() => handleSelecteProvider(provider.id)}
+              selected={provider.id === selectedProvider}
+            >
+              <ProviderAvatar
+                source={{
+                  uri:
+                    provider.avatar_url ||
+                    `https://api.adorable.io/avatar/50/${provider.name}.jpeg`,
+                }}
+              />
+              <ProviderName selected={provider.id === selectedProvider}>
+                {provider.name}
+              </ProviderName>
+            </ProviderContainer>
+          )}
+        />
+      </ProvidersListContainer>
+    </Container>
+  );
+};
+
+export default CreateAppointment;
